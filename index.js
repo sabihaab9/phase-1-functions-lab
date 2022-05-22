afabf735082197c1a3ea3c13a7e4bc16a3c30aa0
Sabiha Bary <sabihaab9@gmail.com>
2022-05-21 21:18:32 -0400

    commit tree 7500f168d7fca532a5680dde4f2bab18935d86da

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    
+    let distanceInBlocks = Math.abs(someValue - 42);
+
+    return distanceInBlocks;
+  }
+
+  function distanceFromHqInFeet(someValue) {
+   let distanceInBlocks = distanceFromHqInBlocks(someValue);
+    let distanceInFeet = distanceInBlocks*264;
+
+    return distanceInFeet;
+    
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    
+    let distanceInBlocks = Math.abs(start - destination);
+    let distanceInFeet = distanceInBlocks*264;
+
+    return distanceInFeet;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    
+    let distanceInFeet = distanceTravelledInFeet(start, destination);
+    console.log(distanceInFeet)
+    if (distanceInFeet>2500){
+
+        return 'cannot travel that far';
+    }
+    else if (distanceInFeet>2000){
+
+       let farePrice = 25;
+       return farePrice;
+
+    }
+    else if(distanceInFeet<=400){
+
+        let farePrice = 0;
+        return farePrice;
+
+    }
+    else{
+
+      let farePrice = (distanceInFeet-400)*0.02;
+      return farePrice;
+    } 
+  }
